5507fe9657b62d81b5c6b0ec5a3e203a441b5949
amitleuva1987 <amitleuva1987@gmail.com>
2022-08-02 12:10:25 +0530

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        if (! Auth::guard('web')
+            ->attempt(
+                request()->only('email', 'password'),
+                request()->boolean('remember')
+            )) {
+            throw ValidationException::withMessages([
+                'email' => 'Authentication failed !',
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+    }
+}
